0eeedd863c00dfd71f8a2c912e9e78bb4450d984
Jasper L <126206478+Swebwite@users.noreply.github.com>
2025-02-06 23:08:55 +0000

    finished calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
-x = float(input('x?'))
-y = float(input('y?'))
+def main():
+    x = int(input('x?'))
+    print('x squared is', square(x))
 
-z = round(x / y, 2)
 
-print(z)
+def square(n):
+    return n ** 2
+
+
+main()
