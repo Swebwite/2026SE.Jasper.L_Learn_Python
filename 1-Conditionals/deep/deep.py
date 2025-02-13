6f7332197b4a15df19f2a766f3ed97a97ded0098
Jasper L <126206478+Swebwite@users.noreply.github.com>
2025-02-13 09:25:42 +0000

    finished deep.py

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,13 @@
+meaning = input('What is the answer to the Great Question of Life, the Universe and Everything? ')
+meaning = meaning.lower()
+x = '42'
+y = 'forty-two'
+z = 'forty two'
+if meaning == x:
+    print('Yes')
+elif meaning == y:
+    print('Yes')
+elif meaning == z:
+    print('Yes')
+else:
+    print('No')
