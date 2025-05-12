f654d38b5c69fcb52716aac5328213c25a3d7c3a
Jasper L <126206478+Swebwite@users.noreply.github.com>
2025-05-12 01:08:07 +0000

    continuing video

--- 8-OOP/student.py
+++ 8-OOP/student.py
@@ -5,30 +5,16 @@ class Student:
 
     def __str__(self):
         return f'{self.name} from {self.house}'
-    
-    @property 
-    def name(self):
-        return self._name
-    
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError('missing name')
-        self._name = name
-    
-    @property
-    def house(self):
-        return self._house
-    
-    @house.setter
-    def house(self, house):
-        if house not in ['one', 'two', 'three', 'four']:
-            raise ValueError('invalid house')
-        self._house = house
+
+    @classmethod
+    def get(cls):
+        name = input('Name: ')
+        house = input('House: ')
+        return cls(name, house)
 
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
 
 
